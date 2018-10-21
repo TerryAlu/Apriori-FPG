e42d5c94a314ff0a8accdc238d5171af65e8c381
TerryAlu <terrycaiyz@gmail.com>
2018-10-21 15:14:56 +0000

    conditional fp tree

--- fpg.py
+++ fpg.py
@@ -9,10 +9,9 @@ def read_data(filepath):
     trans = []
     with open(filepath) as fp:
         for line in fp:
-            trans.append(frozenset({x.strip() for x in line.split(',')}))
-    itemsets = {frozenset([y]) for x in trans for y in x}
+            trans.append({x.strip() for x in line.split(',')})
 
-    return itemsets, trans
+    return trans
 
 def count_freq(count_dict, set_list, trans):
     """
@@ -22,16 +21,44 @@ def count_freq(count_dict, set_list, trans):
         count_dict[x] = 0
     for x in set_list:
         for y in trans:
-            if x.issubset(y):
+            if x in y:
                 count_dict[x] = count_dict[x] + 1
 
-def support(count_dict, item, trans):
+
+def sorted_mincount_trans(trans, mincount, subtree=False):
     """
-    Return support of the item
-    Notice: count_dict need to be init. before this function by count_freq()
+    Remove items from transactions if they do not satisfy minimum count condition.
+    Rearrange item order for each transaction accroding to the frequence of one-item set.
+    If this function is called by subtree, then there is no need to rearrange item order.
     """
-    item_frozenset = frozenset(item)
-    return 1.0*count_dict.get(item_frozenset, 0)/len(trans)
+
+    # trans to itemset
+    itemsets = {y for x in trans for y in x}
+
+    ## Create sorted one-item list
+    count_dict = {}
+    # one item count
+    count_freq(count_dict, itemsets, trans)
+    # check mincount
+    freq_items = {k: v for k, v in count_dict.iteritems() if v >= mincount}
+    # sort one-item by count
+    sorted_freq_list = sorted(freq_items.iteritems(), key=lambda x: x[1], reverse=True)
+    # assign index of elements in freq_list
+    freq_list_index_map = {}
+    for i, x in enumerate(sorted_freq_list):
+        freq_list_index_map[x[0]] = i
+
+    ## Modify and reorder all trans. according to sorted one-item list
+    # create set of freq. one item
+    freq_one_item_set = {x for x, y in sorted_freq_list}
+    # delete unfreq, items and represent sorted trans. by 2d list
+    sorted_trans = [list(trans_set & freq_one_item_set) for trans_set in trans]
+    # rearrange trans. by sorted_freq_list
+    if not subtree:
+        for idx, arr in enumerate(sorted_trans):
+            sorted_trans[idx] = sorted(arr, key=lambda x: freq_list_index_map[x])
+
+    return sorted_freq_list, sorted_trans
 
 class Node(dict):
     """
@@ -45,6 +72,9 @@ class Node(dict):
         self.child = {} # str => Node
         self.count = 0
 
+    def is_root(self):
+        return self.name is None
+
     def __str__(self):
         # Root
         if self.name is None:
@@ -52,7 +82,7 @@ class Node(dict):
         elif self.parent is None:
             return "Node<"+ self.name + ": ?>"
         else:
-            return "Node<"+ self.name + ": %s>" % self.parent.name if self.parent.name else "_Root_"
+            return "Node<"+ self.name + ": %s>" % (self.parent.name if self.parent.name else "_Root_")
 
     def __repr__(self):
         return self.__str__()
@@ -66,53 +96,120 @@ class Node(dict):
     def __lt__(self, other):
         return self.count < other.count
 
-def connect(parent, child):
+class Tree(dict):
     """
-    Connect parent and child nodes by setting attr. of nodes
+    FP Grwoth tree.
     """
-    parent.child[child] = child
-    child.parent = parent
-    child.count = child.count + 1
 
-def subset(ori):
-    ret = list(chain.from_iterable([combinations(ori, i) for i in xrange(1, len(ori)+1)]))
-    return [frozenset(x) for x in ret]
+    def __init__(self, trans, threshold, subtree_item=None):
+        """
+        If the subtree_item is not None, then the tree is a conditional tree,
+        and the threshold will be minimum count as opposed to minimum support.
+        Otherwise, the tree is not a conditional tree, the threshold = minsup.
+        """
+        self.trans = trans
+        if subtree_item is not None:
+            self.mincount = threshold
+        else:
+            self.mincount = threshold * len(trans)
+        self.subtree_item = subtree_item # type = Node
 
-def traceback_mincount(traceback):
-    """
-    Return list of tuple (subset, mincount) of nodes
-    """
-    item_node = traceback[0]
-    # sort traceback nodes
-    sorted_traceback = sorted([node for node in traceback[1:]])
-    # generate subset of traceback nodes
-    traceback_subset = subset({node.name for node in traceback[1:]})
-    # generate return list
-    ret = []
-    for x in traceback_subset:
-        for y in sorted_traceback:
-            # compare the smallest count of traceback[1:] and count of item node
-            if y.name in x:
-                mincount = y.count if y.count<item_node.count else item_node.count
-                ret.append((x.union({item_node.name}), mincount))
-                break
-    return ret
+        self.horizon_access = defaultdict(list) # {name: [node...]}
+        self.root = None
+        self.sf_items, self.sm_trans = sorted_mincount_trans(self.trans, self.mincount, self.subtree_item is not None)
+        self.build()
 
-def traceback_to_patterns(traceback_list, minsup, trans):
-    """
-    Return dict. of freq. patterns according to a list of traceback. eg. {forzenset pattern: count}
-    traceback: list contains all nodes from item node to the one before root node. eg. [node...]
-    """
-    patterns = defaultdict(int)
-    # count all subsets
-    for traceback in traceback_list:
-        for ss, mincount in traceback_mincount(traceback):
-            patterns[ss] = patterns[ss] + mincount
-    # check minsup
-    for k, v in patterns.copy().iteritems():
-        if 1.0*v/len(trans) < minsup:
-            del patterns[k]
-    return patterns
+    def build(self):
+        """
+        Create fp tree accroding to trans. and record horizontal access order (i.e header list).
+        """
+        self.root = Node()
+        for tran in self.sm_trans:
+            cur_node = self.root
+            for x in tran:
+                # try to get old node
+                next_node = cur_node.child.get(x, None)
+                # if new node is created
+                if next_node is None:
+                    next_node = Node(x)
+                    self.horizon_access[x].append(next_node)
+                # connect and count
+                self.connect(cur_node, next_node)
+                # move to next node
+                cur_node = next_node
+        # print "\n\n----"
+        # print self.print_tree(self.root)
+
+    def print_tree(self, root):
+        print root
+        for x in root.child.values():
+            self.print_tree(x)
+
+    def find_patterns(self):
+        """
+        Find all patterns satisfy the minimum support and return them as a list of tuple.
+        For each 1 freqent item, if this tree has branch, then it will construct conditional subtree recusively
+        until trivial enough to find trivial patterns.
+        """
+        ret = []
+        if self.has_branch():
+            for item, _ in reversed(self.sf_items):
+                item_linkedlist = self.horizon_access[item]
+                subtree_trans = []
+                for item_node in item_linkedlist:
+                    count = item_node.count
+                    item_node = item_node.parent
+                    traceback = set() # list of traceback node name (exclude the btn item)
+                    while not item_node.is_root():
+                        traceback.add(item_node.name)
+                        item_node = item_node.parent
+                    for x in xrange(count):
+                        subtree_trans.append(traceback)
+                subtree = Tree(subtree_trans, self.mincount, item)
+                ret.extend(subtree.find_patterns())
+
+        else:
+            ret = self.trivial_patterns()
+
+        # add subtree condition item
+        if self.subtree_item is not None:
+            for i, t in enumerate(ret):
+                ret[i] = t + (self.subtree_item,)
+            ret.append((self.subtree_item,))
+        return ret
+
+
+    def subset(self, ori):
+        ret = list(chain.from_iterable(combinations(ori, i) for i in xrange(1, len(ori)+1)))
+        return ret
+
+    def trivial_patterns(self):
+        """
+        Return a list of patterns (tuple) if the tree does not have branch
+        """
+        trace = []
+        assert self.root is not None
+        current_node = self.root
+        while len(current_node.child)>0:
+            current_node = current_node.child.values()[0]
+            trace.append(current_node.name)
+        ret = self.subset(trace)
+        return ret
+
+    def has_branch(self):
+        assert self.root is not None
+        current_node = self.root
+        while len(current_node.child)==1:
+            current_node = current_node.child.values()[0]
+        return len(current_node.child)>1 
+
+    def connect(self, parent, child):
+        """
+        Connect parent and child nodes by setting attr. of nodes
+        """
+        parent.child[child.name] = child
+        child.parent = parent
+        child.count = child.count + 1
 
 def set_string(itemset):
     return "(" + ", ".join(x for x in itemset) + ")"
@@ -120,8 +217,10 @@ def set_string(itemset):
 def print_result(patterns):
     # print freq. itemset
     print ">> Frequent Itemsets <<"
-    for pattern in patterns.keys():
+    patterns = [x for x in sorted(patterns, key=lambda x: len(x))]
+    for pattern in patterns:
         print set_string(pattern)
+
 
 def cmd():
     # Parse options
@@ -130,67 +229,13 @@ def cmd():
     parser.add_argument('-s', '--minsup', help='minumum support (0~1)', dest='minsup', default=0.5, type=float)
     options = parser.parse_args()
 
-    itemsets, trans = read_data(options.filepath)
+    trans = read_data(options.filepath)
 
     ## FP-Growth Algorithm
     
-    ## Create sorted one-item list
-    count_dict = {}
-    # one item count
-    count_freq(count_dict, itemsets, trans)
-    # sort one-item by count
-    sorted_freq_list = sorted(count_dict.iteritems(), key=lambda x: x[1], reverse=True)
-    # check minsup
-    sorted_freq_list = filter(lambda x: support(count_dict, x[0], trans) >= options.minsup, sorted_freq_list)
-    # assign index of elements in freq_list
-    freq_list_index_map = {}
-    for i, x in enumerate(sorted_freq_list):
-        freq_list_index_map[x[0]] = i
-
-    ## Modify and reorder all trans. according to sorted one-item list
-    # create set of freq. one item
-    freq_one_item_set = {x for frozenset_count in sorted_freq_list for x in frozenset_count[0]}
-    # represent sorted trans. by 2d list
-    sorted_trans = [list(trans_set & freq_one_item_set) for trans_set in trans]
-    # delete and rearrange trans. by sorted_freq_list
-    for idx, arr in enumerate(sorted_trans):
-        sorted_trans[idx] = sorted(arr, key=lambda x: freq_list_index_map[frozenset([x])])
-
-    ## Construct FP-Tree
-    horizon_access = defaultdict(list) # {name: [node...]}
-    root = Node()
-    for tran in sorted_trans:
-        cur_node = root
-        for x in tran:
-            # try to get old node
-            next_node = cur_node.child.get(x, None)
-            # if new node is created
-            if next_node is None:
-                next_node = Node(x)
-                horizon_access[x].append(next_node)
-            # connect and count
-            connect(cur_node, next_node)
-            # move to next node
-            cur_node = next_node
-
-    ## Generate patterns from item
-    patterns = {}
-    for item_name, item_linkedlist in horizon_access.iteritems():
-        traceback_list = []
-        for item_node in item_linkedlist:
-            traceback = []
-            while item_node is not None:
-                traceback.append(item_node)
-                item_node = item_node.parent
-            traceback_list.append(traceback)
-        patterns.update(traceback_to_patterns(traceback_list, options.minsup, trans))
-    
-    # update freq. one-itemset
-    for one_itemset, count in sorted_freq_list:
-        patterns[one_itemset] = count
-
-    ## print all freq. itemsets
-    print_result(patterns)
+    fp = Tree(trans, options.minsup)
+    pats = fp.find_patterns()
+    print_result(pats)
 
 if __name__ == '__main__':
     cmd();
